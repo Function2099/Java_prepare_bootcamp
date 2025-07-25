6e624f5051cd754930fa04edac6749f2e51bf5c5
Function_range <s36164296@gmail.com>
2025-07-25 18:26:08 +0800

    上傳測試

--- 階段專案一/HelloWorld.java
+++ 階段專案一/HelloWorld.java
@@ -1,7 +1,8 @@
-package 階段一;
+package 階段專案一;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("測試git add .(修改)和 git commit -m '更新日誌' 和上傳git push");
     }
 }
 
